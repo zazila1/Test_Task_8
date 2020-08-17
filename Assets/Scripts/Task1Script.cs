e036e5d2c64a8f9911a7b10af23546940a339850
zazila1 <zazila1@gmail.com>
2020-08-17 13:42:53 +0300

    UI, задание 1

---
+++ Assets/Scripts/Task1Script.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Task1Script : MonoBehaviour
+{
+    [SerializeField] private InputField _Number;
+    
+    [SerializeField] private Text _Result1;
+    [SerializeField] private Text _Result2;
+    [SerializeField] private Text _Result3;
+    
+    public void CheckAll()
+    {
+        int num = Int32.Parse(_Number.text);
+
+        _Result3.text = Check3(num).ToString();
+
+    }
+
+    private bool Check3(int number)
+    {
+        while((number % 4) == 0)
+        {
+
+            if ((number /= 4) == 1)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
